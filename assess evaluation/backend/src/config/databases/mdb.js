c530c0decddd387dc33d124c7e256db57519454d
mayurawari <117013117+mayurawari@users.noreply.github.com>
2024-07-08 19:09:57 +0530

    updated final

--- assess evaluation/backend/src/config/databases/mdb.js
+++ assess evaluation/backend/src/config/databases/mdb.js
@@ -1,11 +1,17 @@
 import { Sequelize } from "sequelize";
+import {config} from "dotenv";
+config();
+const port=process.env.SQ_PORT;
+const user=process.env.SQ_USER;
+const password=process.env.SQ_PASSWORD;
+const host=process.env.SQ_HOST;
 
 
-const connectsql=new Sequelize("store","avnadmin","AVNS_EgrQ8zHGcvli1NFd9O1",{
-   host:"mysql-32205d85-mayurawari50-cb28.h.aivencloud.com",
-   port:"11161",
+const connectsql=new Sequelize("store",`${user}`,`${password}`,{
+   host:`${host}`,
+   port:`${port}`,
    dialect:"mysql"
-   
+
 });
 
-export default connectsql;
+export default connectsql; 
