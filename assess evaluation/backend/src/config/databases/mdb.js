ec244cafa840d408fea8727c48bead06836eb8cd
mayurawari <117013117+mayurawari@users.noreply.github.com>
2024-07-08 19:01:59 +0530

    updated

--- assess evaluation/backend/src/config/databases/mdb.js
+++ assess evaluation/backend/src/config/databases/mdb.js
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
 
 
-const connectsql=new Sequelize("store","root","password",{
-   host:"localhost",
+const connectsql=new Sequelize("store","avnadmin","AVNS_EgrQ8zHGcvli1NFd9O1",{
+   host:"mysql-32205d85-mayurawari50-cb28.h.aivencloud.com",
+   port:"11161",
    dialect:"mysql"
+   
 });
 
 export default connectsql;
